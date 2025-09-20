302ab7180e27ba842654c11631581a8b538019f6
salmafishar <salmafisher@icloud.com>
2025-09-19 23:18:13 -0600

    phase 0 implementation

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Objects;
+
 /**
  * Represents moving a chess piece on a chessboard
  * <p>
@@ -47,5 +49,15 @@ public class ChessMove {
     public String toString() {
         return String.format("%s,%s", startPosition, endPosition);
     }
-
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof ChessMove that)) {
+            return false;
+        }
+        return startPosition.equals(that.startPosition) && endPosition.equals(that.endPosition) && promotionPiece == that.promotionPiece;
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(startPosition, endPosition, promotionPiece);
+    }
 }
