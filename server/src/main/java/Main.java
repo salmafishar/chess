b93592834665d2362d2bd699a3fedab9839d0005
salmafishar <salmafisher@icloud.com>
2025-10-21 21:35:37 -0600

    code for testing the endpoints while implementing the phase

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -3,7 +3,9 @@ import server.Server;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Server: " + piece);
+        Server server = new Server();
+        server.run(8080);
+
+        System.out.println("♕ 240 Chess Server");
     }
 }
