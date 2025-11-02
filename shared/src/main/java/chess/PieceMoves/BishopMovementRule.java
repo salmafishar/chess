a75c410c66911ec49fe41b9d7f341c8065944431
salmafishar <salmafisher@icloud.com>
2025-11-02 16:34:24 -0700

    implemented bishop & queen

---
+++ shared/src/main/java/chess/PieceMoves/BishopMovementRule.java
@@ -0,0 +1,28 @@
+package chess.PieceMoves;
+
+import chess.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/*
+Bishops move in diagonal lines as far as there is open space.
+If there is an enemy piece at the end of the diagonal, the bishop may move to that position and capture the enemy piece.
+
+
+ */
+public class BishopMovementRule extends BaseMovement {
+    @Override
+    public Collection<ChessMove> moves(ChessBoard board, ChessPosition position) {
+        ArrayList<ChessMove> moves = new ArrayList<>();
+        int r = position.getRow();
+        int c = position.getColumn();
+        ChessPiece me = at(board, r, c);
+        ChessGame.TeamColor color = me.getTeamColor();
+        int[][] directions = {
+                {1, 1}, {1, -1}, {-1, 1}, {-1, -1},
+        };
+        addSlidingMove(position, board, directions, r, c, color, moves);
+        return moves;
+    }
+}
