38fbb0ded8a8029026cf0bdefdf6a9794b1fb13d
salmafishar <salmafisher@icloud.com>
2025-11-03 10:43:12 -0700

    LoginHandler

--- server/src/main/java/server/handlers/LoginHandler.java
+++ server/src/main/java/server/handlers/LoginHandler.java
@@ -3,45 +3,40 @@ package server.handlers;
 import com.google.gson.Gson;
 import dataaccess.DataAccessException;
 import io.javalin.http.Context;
-import io.javalin.http.Handler;
-import org.jetbrains.annotations.NotNull;
 import service.UserService;
 import service.requests.LoginRequest;
+import service.results.LoginResult;
 
 import java.util.Map;
 
-public class LoginHandler implements Handler {
-    private final UserService userService;
-    private final Gson gson = new Gson();
+public class LoginHandler {
+    private final Gson gson;
+    private final UserService user;
 
-    public LoginHandler(UserService userService) {
-        this.userService = userService;
+    public LoginHandler(Gson gson, UserService user) {
+        this.gson = gson;
+        this.user = user;
     }
 
-    @Override
-    public void handle(@NotNull Context context) throws Exception {
+    public void login(Context ctx) {
         try {
-            LoginRequest request = gson.fromJson(context.body(), LoginRequest.class);
-            var result = userService.login(request);
-            context.status(200);
-            context.contentType("application/json");
-            context.result(gson.toJson(result));
+            var request = gson.fromJson(ctx.body(), LoginRequest.class);// name, pass, email
+            var result = user.login(request);
 
+            ctx.status(200).contentType("application/json").
+                    result(gson.toJson(result, LoginResult.class));
         } catch (DataAccessException e) {
-            String message = e.getMessage();
+            String message = e.getMessage().toLowerCase();
             if (message.contains("bad request")) {
-                context.status(400).contentType("application/json")
+                ctx.status(400).contentType("application/json")
                         .result(gson.toJson(Map.of("message", "Error: bad request")));
-            } else if (message.toLowerCase().contains("unauthorized")) {
-                context.status(401).contentType("application/json")
+            } else if (message.contains("unauthorized") || message.contains("not found")) {
+                ctx.status(401).contentType("application/json")
                         .result(gson.toJson(Map.of("message", "Error: unauthorized")));
             } else {
-                context.status(500).contentType("application/json")
-                        .result(gson.toJson(Map.of("message", "Error: " + e.getMessage())));
+                ctx.status(500).contentType("application/json")
+                        .result(gson.toJson(Map.of("message", "Error: " + message)));
             }
-        } catch (Exception e) {
-            context.status(500).contentType("application/json")
-                    .result(gson.toJson(Map.of("message", "Error: " + e.getMessage())));
         }
     }
 }
