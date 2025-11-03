8441fb6dd5fc974068307f6024ecab8af3ea8bf2
salmafishar <salmafisher@icloud.com>
2025-11-02 22:12:24 -0700

    added request classes

---
+++ server/src/main/java/service/requests/JoinGameRequest.java
@@ -0,0 +1,4 @@
+package service.requests;
+
+public record JoinGameRequest(String authToken, String playerColor, int gameID) {
+}
---
+++ server/src/main/java/service/requests/ListGameRequest.java
@@ -0,0 +1,4 @@
+package service.requests;
+
+public record ListGameRequest(String authToken) {
+}
