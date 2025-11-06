1a8f1988fdb120e64f253cd2edb75f4259060d50
salmafishar <salmafisher@icloud.com>
2025-11-05 23:11:41 -0700

    added test files for clear and UserService

---
+++ server/src/test/java/service/UserServiceTest.java
@@ -0,0 +1,85 @@
+package service;
+
+import dataaccess.DataAccessException;
+import dataaccess.MemoryDataAccess;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import service.requests.LoginRequest;
+import service.requests.LogoutRequest;
+import service.requests.RegisterRequest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+//tests call service methods then assert results/exceptions
+
+class UserServiceTest {
+    MemoryDataAccess dao;
+    UserService user;
+
+
+    @BeforeEach
+    void clear() throws DataAccessException {
+        dao = new MemoryDataAccess();
+        user = new UserService(dao);
+        dao.clear();
+    }
+
+    @Test
+        // Positive Test
+        // returns auth token
+    void register_ok() throws DataAccessException {
+        var req = new RegisterRequest("sal", "21", "s@al21");
+        var res = user.register(req);
+        assertNotNull(res.authToken());
+        assertEquals("sal", res.username());
+    }
+
+    @Test
+    void register_name_duplicate() throws DataAccessException {
+        user.register(new RegisterRequest("sal", "21", "s@al21"));
+        DataAccessException ex = assertThrows(DataAccessException.class, () ->
+                user.register(new RegisterRequest("sal", "21", "s@al21")));
+        assertEquals("Username Already exists", ex.getMessage());
+    }
+
+    @Test
+    void login_ok() throws DataAccessException {
+        var registerReq = new RegisterRequest("sal", "21", "s@al21");
+        user.register(registerReq);
+        var req = new LoginRequest("sal", "21");
+        var res = user.login(req);
+        assertNotNull(res.authToken());
+        assertEquals("sal", res.username());
+    }
+
+    @Test
+    void login_failed() throws DataAccessException {
+        var registerReq = new RegisterRequest("sal", "21", "s@al21");
+        user.register(registerReq);
+        var req = new LoginRequest("sal", "21");
+        var res = user.login(req);
+        DataAccessException ex = assertThrows(DataAccessException.class, () ->
+                user.login(new LoginRequest("sl", "21")));
+        assertEquals("unauthorized", ex.getMessage());
+    }
+
+    @Test
+    void logout_ok() throws DataAccessException {
+        var registerReq = user.register(new RegisterRequest("sal", "21", "s@al21"));
+        var token = registerReq.authToken();
+        var logoutReq = new LogoutRequest(token);
+        user.logout(logoutReq);
+        var ex = assertThrows(DataAccessException.class, () ->
+                user.logout(logoutReq));
+        assertEquals("Unauthorized", ex.getMessage());
+    }
+
+    @Test
+    void logout_failed() throws DataAccessException {
+        var registerReq = user.register(new RegisterRequest("sal", "21", "s@al21"));
+        var name = registerReq.username();
+        var ex = assertThrows(DataAccessException.class, () ->
+                user.logout(new LogoutRequest(name)));
+        assertEquals("Unauthorized", ex.getMessage());
+    }
+}
