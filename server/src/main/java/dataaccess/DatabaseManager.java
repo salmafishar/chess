43598a8ed3c09a30657916c8c945f1ff03ce3122
salmafishar <salmafisher@icloud.com>
2025-11-06 20:10:03 -0700

    syntax error

--- server/src/main/java/dataaccess/DatabaseManager.java
+++ server/src/main/java/dataaccess/DatabaseManager.java
@@ -49,7 +49,7 @@ public class DatabaseManager {
               `username` varchar(256) NOT NULL,
               `email` varchar(256) NOT NULL,
               `password` varchar(256) NOT NULL,
-              PRIMARY KEY (`username`),
+              PRIMARY KEY (`username`)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
             """,
             """
