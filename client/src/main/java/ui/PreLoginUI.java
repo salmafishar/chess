3c325a70019903a63a948302b67adcef11d00b5e
salmafishar <salmafisher@icloud.com>
2025-11-16 19:40:46 -0700

    register

--- client/src/main/java/ui/PreLoginUI.java
+++ client/src/main/java/ui/PreLoginUI.java
@@ -14,4 +14,17 @@ public class PreLoginUI implements ClientUI {
     public String handle(String cmd, String[] params) {
         return "";
     }
+
+    public String register(String[] params) throws DataAccessException {
+        if (params.length != 3) {
+            return "To register, please type in: register <USERNAME> <PASSWORD> <EMAIL>";
+        }
+        String name = params[0];
+        String password = params[1];
+        String email = params[2];
+        var registerRequest = new RegisterRequest(name, password, email);
+        var register = server.register(registerRequest);
+        repl.switchToPostLogin();
+        return String.format("You are now logged in. Your username is %s", register.username());
+    }
 }
