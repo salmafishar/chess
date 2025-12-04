c00443331bf6e58a2f8d411042a987460c5bf1fe
salmafishar <salmafisher@icloud.com>
2025-12-04 16:30:26 -0700

    fixed server back to og

--- server/src/main/java/dataaccess/DataAccessException.java
+++ server/src/main/java/dataaccess/DataAccessException.java
@@ -2,6 +2,7 @@ package dataaccess;
 
 import com.google.gson.Gson;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -25,6 +26,20 @@ public class DataAccessException extends Exception {
         this.code = code;
     }
 
+    public static DataAccessException fromJson(String json) {
+        var map = new Gson().fromJson(json, HashMap.class);
+        var status = Code.valueOf(map.get("status").toString());
+        String message = map.get("message").toString();
+        return new DataAccessException(status, message);
+    }
+
+    public static Code fromHttpStatusCode(int httpStatusCode) {
+        return switch (httpStatusCode) {
+            case 500 -> Code.ServerError;
+            case 400 -> Code.ClientError;
+            default -> throw new IllegalArgumentException("Unknown HTTP status code: " + httpStatusCode);
+        };
+    }
 
     public Code code() {
         return code;
@@ -34,6 +49,12 @@ public class DataAccessException extends Exception {
         return new Gson().toJson(Map.of("message", getMessage(), "status", code));
     }
 
+    public int toHttpStatusCode() {
+        return switch (code) {
+            case ServerError -> 500;
+            case ClientError -> 400;
+        };
+    }
 
     public enum Code {
         ServerError,
