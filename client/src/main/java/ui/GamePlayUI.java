8cf9edd476825f4d48397cdb64e5c02310cbbeb1
salmafishar <salmafisher@icloud.com>
2025-12-05 16:12:11 -0700

    skeleton for GamePlayUI

---
+++ client/src/main/java/ui/GamePlayUI.java
@@ -0,0 +1,104 @@
+package ui;
+
+/*
+    sends commands to sever through ws
+    draw the current state of the chess board from the side the user is playing.
+
+    Command >> Description
+    Help >> Displays text informing the user what actions they can take.
+
+    Redraw Chess Board >> Redraws the chess board upon the user’s request.
+
+    Leave >> Removes the user from the game (whether they are playing or observing the game).
+        The client transitions back to the Post-Login UI.
+
+    Make Move >> Allow the user to input what move they want to make.
+        The board is updated to reflect the result of the move,
+        and the board automatically updates on all clients involved in the game.
+
+    Resign >> Prompts the user to confirm they want to resign.
+        If they do, the user forfeits the game and the game is over.
+        Does not cause the user to leave the game.
+
+    Highlight Legal Moves >> Allows the user to input the piece for which they want to highlight legal moves.
+        The selected piece’s current square and all squares it can legally move to are highlighted.
+        This is a local operation and has no effect on remote users’ screens.
+ */
+
+import chess.ChessGame;
+import websocket.ServerMessageHandler;
+import websocket.WebSocketFacade;
+import websocket.messages.ServerMessage;
+
+public class GamePlayUI implements ClientUI, ServerMessageHandler {
+    private String authToken = null;
+    private final WebSocketFacade ws;
+    private ChessGame.TeamColor myColor;
+    private ChessGame currentGame;
+    private final int gameID;
+    private final String serverUrl;
+
+    public GamePlayUI(String authToken, String serverUrl, int gameID, WebSocketFacade ws, ChessGame.TeamColor myColor) {
+        this.ws = ws;
+        this.gameID = gameID;
+        this.serverUrl = serverUrl;
+    }
+
+    public void setAuthToken(String token) {
+        this.authToken = token;
+    }
+
+    @Override
+    public String handle(String cmd, String[] params) throws Exception {
+        if (cmd.equalsIgnoreCase("help")) {
+            return """
+                    - move <from> <to>
+                    - resign
+                    - leave
+                    - highlight <square>
+                    - help
+                    """;
+        }
+        if (cmd.equalsIgnoreCase("redraw")) {
+            return redrawChessBoard(params);
+        }
+        if (cmd.equalsIgnoreCase("leave")) {
+            return leave(params);
+        }
+        if (cmd.equalsIgnoreCase("move")) {
+            return makeMove(params);
+        }
+        if (cmd.equalsIgnoreCase("resign")) {
+            return resign(params);
+        }
+        if (cmd.equalsIgnoreCase("highlight")) {
+            return highlight(params);
+        }
+        return "Unknown command. Type 'help' to see available commands.";
+    }
+
+    private String redrawChessBoard(String[] params) {
+        return null;
+    }
+
+    private String leave(String[] params) {
+        return null;
+    }
+
+    private String makeMove(String[] params) {
+        return null;
+    }
+
+    private String resign(String[] params) {
+        return null;
+    }
+
+    private String highlight(String[] params) {
+        return null;
+    }
+
+    @Override
+    public void notify(ServerMessage message) {
+
+    }
+}
